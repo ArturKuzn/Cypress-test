43e61138de57d36641c8b85d4bc2d7fed8fea426
Artur Kuznetsov <artur.kuznetsov1990@gmail.com>
2023-05-13 16:19:08 +0300

    add initial files

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,14 @@
+describe('Test suite', () => {
+  it.skip('Test 1', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Test 2', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Test 3', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+})
