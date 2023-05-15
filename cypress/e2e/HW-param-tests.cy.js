d133acfa43e3bd365ea6074e9e2f33181c5b259d
Artur Kuznetsov <artur.kuznetsov1990@gmail.com>
2023-05-15 20:19:20 +0300

    add expected result

--- cypress/e2e/HW-param-tests.cy.js
+++ cypress/e2e/HW-param-tests.cy.js
@@ -6,10 +6,12 @@ describe('Multiple tests', () => {
         cy.get('[title="Form Layouts"]').click();
       })
 
-    const test = (testData1, testData2) =>
+    const test = (testData1, testData2,expectedData1, expectedData2) =>
     function () {
       cy.get('[placeholder="Jane Doe"]').type(`${testData1}`);
+      cy.get('[placeholder="Jane Doe"]').should("contain.value", `${expectedData1}`)
       cy.get('input[type="text"][placeholder="Email"]').type(`${testData2}`);
+      cy.get('input[type="text"][placeholder="Email"]').should("contain.value", `${expectedData2}`)
       cy.get('.custom-checkbox').first().click();
       cy.get('[type="submit"]').eq(0).click();
     };
@@ -18,7 +20,7 @@ describe('Multiple tests', () => {
       })
        
 
-  it('Test for User 1', test('User 1', 'user1@example.com'));
-  it('Test for User 2', test('User 2', 'user2@example.com'));
-  it('Test for User 3', test('User 3', 'user3@example.com'));
+  it('Test for User 1', test('User 1', 'user1@example.com','User 1', 'user1@example.com'));
+  it('Test for User 2', test('User 2', 'user2@example.com', 'User 2', 'user2@example.com'));
+  it('Test for User 3', test('User 3', 'user3@example.com', 'User 3', 'user3@example.com'));
 })
